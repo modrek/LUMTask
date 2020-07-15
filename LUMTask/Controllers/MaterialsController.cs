fb36a2042d200d200e53e766ef53af3b2cdf330c
= <=>
2020-07-15 18:58:50 +0200

    Remove Validate Methos from MaterialsControllers

--- LUMTask/Controllers/MaterialsController.cs
+++ LUMTask/Controllers/MaterialsController.cs
@@ -43,13 +43,6 @@ namespace LUMTask.Controllers
         [HttpPost]
         public IActionResult Post([FromBody] MaterialModel model)
         {
-            if (!ModelState.IsValid)           // Invokes the build-in
-                return BadRequest(ModelState); //
-
-            Validate(model);
-
-            if (!ModelState.IsValid)
-                return Ok(String.Join("\n", ModelState.Values.Where(x => x.ValidationState == ModelValidationState.Invalid).Select(x => x.Errors[0].ErrorMessage).ToList()));
 
             _materialRepository.Add(model);
             _materialRepository.Complete();
@@ -58,16 +51,10 @@ namespace LUMTask.Controllers
         }
 
 
-
         // PUT api/<MaterialController>/5
         [HttpPut("{id}")]
         public IActionResult Put(string id, [FromBody] MaterialModel model)
         {
-            Validate(model);
-
-            if (!ModelState.IsValid)
-                return Ok(String.Join("\n", ModelState.Values.Where(x => x.ValidationState == ModelValidationState.Invalid).Select(x => x.Errors[0].ErrorMessage).ToList()));
-
 
             _materialRepository.Update($"MaterialModels/{id}", model);
             _materialRepository.Complete();
@@ -86,34 +73,7 @@ namespace LUMTask.Controllers
             _materialRepository.Complete();
         }
 
-        private void Validate(MaterialModel model)
-        {
-            if (string.IsNullOrEmpty(model.MaterialName))
-                ModelState.AddModelError("MaterialName", "MaterialName is required.");
-
-            if (string.IsNullOrEmpty(model.Author))
-                ModelState.AddModelError("Author", "Author is required.");
-
-            if (string.IsNullOrEmpty(model.Note))
-                ModelState.AddModelError("Note", "Note is required.");
-
-            if (!Enum.IsDefined(typeof(TypeOfPhase), model.Type))
-                ModelState.AddModelError("Type", "Type must be ......");
-
-            if (model.Visible == null)
-                ModelState.AddModelError("Visible", "Visible is required.");
-
-
-            if (!Between(model.MaterialFunction.Min))
-                ModelState.AddModelError("MaterialFunction.Min", "MaterialFunction.Min must be in valid range.");
-
-            if (!Between(model.MaterialFunction.Max))
-                ModelState.AddModelError("MaterialFunction.Max", "MaterialFunction.Max must be in valid range.");
-        }
-        public static bool Between(float number)
-        {
-            return number >= (Consts.MinValue - Consts.Step) && number <= (Consts.MaxValue +  Consts.Step);
-        }
+        
     }
       
 }
